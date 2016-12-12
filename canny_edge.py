644ab194172aa644d8377948290b86ff9cf1d987
Akbar Gumbira <akbargumbira@gmail.com>
2016-12-12 10:36:02 +0100

    First commit. Adding canny edge detection

---
+++ canny_edge.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot
+
+img = cv2.imread('test/palm1.jpeg', 0)
+edges = cv2.Canny(img, 100, 200)
+
+pyplot.subplot(121), pyplot.imshow(img, cmap='gray')
+pyplot.title('Original Image'), pyplot.xticks([]), pyplot.yticks([])
+pyplot.subplot(122), pyplot.imshow(edges, cmap='gray')
+pyplot.title('Edge image'), pyplot.xticks([]), pyplot.yticks([])
+pyplot.show()	
