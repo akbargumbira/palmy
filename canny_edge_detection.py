8b396bff4b60255e07f6d9e0f4a5e0e6105fbf75
Akbar Gumbira <akbargumbira@gmail.com>
2016-12-12 17:56:32 +0100

    Move the threshold parameter.

--- canny_edge_detection.py
+++ canny_edge_detection.py
@@ -4,7 +4,13 @@ import numpy as np
 
 
 class CannyEdgeDetection(object):
+    MINIMUM_THRESHOLD = 50
+    MAXIMUM_THRESHOLD = 100
+
     def __init__(self, img_src):
         self._img_src = img_src
         self.img = cv2.imread(self._img_src, 0)
-        self.edges = cv2.Canny(self.img, 50, 80)
+        self.edges = cv2.Canny(
+            self.img,
+            CannyEdgeDetection.MINIMUM_THRESHOLD,
+            CannyEdgeDetection.MAXIMUM_THRESHOLD)
