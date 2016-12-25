745484169c74ab418e9e7f0ada7546d6098fc0cb
Akbar Gumbira <akbargumbira@gmail.com>
2016-12-25 19:55:41 +0100

    Add fingertips detection

--- extract_image.py
+++ extract_image.py
@@ -1,6 +1,9 @@
 # coding=utf-8
+import math
 import cv2
+import numpy as np
 from skin_detector import detect_skin
+import sys
 
 
 def extract_image(input_image):
@@ -18,7 +21,97 @@ def extract_image(input_image):
     palm_contour = sorted(contours, key=cv2.contourArea, reverse=True)[0]
 
     # Approximate for better contour
-    epsilon = 0.002*cv2.arcLength(palm_contour, True)
+    epsilon = 0.0015*cv2.arcLength(palm_contour, True)
     palm_contour = cv2.approxPolyDP(palm_contour, epsilon, True)
 
-    return palm_contour
+    # Get the palm center
+    palm_center = find_palm_center(palm_contour)
+    palm_size = 2 * cv2.pointPolygonTest(palm_contour, palm_center, True)
+
+    # Get fingertips
+    fingertips = get_fingertips(palm_contour, input_image)
+    return palm_contour, palm_center, palm_size, fingertips
+
+
+def find_palm_center(palm_contour):
+    """Adapted from https://github.com/VasuAgrawal/GestureDetection/blob/master
+    /current_src/GesturesApi.py."""
+    M = cv2.moments(palm_contour)
+    moment = (int(M['m10']/M['m00']), int(M['m01']/M['m00']))
+    # Create bounding with center the max_point and find better center
+    scale_factor = 0.3
+    shrunk = np.array(palm_contour * scale_factor, dtype=np.int32)
+    tx, ty, w, h = cv2.boundingRect(shrunk)
+    max_point = None
+    max_radius = 0
+    for x in xrange(w):
+        for y in xrange(h):
+            rad = cv2.pointPolygonTest(shrunk, (tx + x, ty + y), True)
+            if rad > max_radius:
+                max_point = (tx + x, ty + y)
+                max_radius = rad
+
+    real_center = np.array(np.array(max_point) / scale_factor, dtype=np.int32)
+    error = int((1 / scale_factor) * 1.5)
+    max_point = None
+    max_radius = 0
+    for x in xrange(real_center[0] - error, real_center[0] + error):
+        for y in xrange(real_center[1] - error, real_center[1] + error):
+            rad = cv2.pointPolygonTest(palm_contour, (x, y), True)
+            if rad > max_radius:
+                max_point = (x, y)
+                max_radius = rad
+
+    return (max_point[0]+moment[0])/2, (max_point[1]+moment[1])/2
+
+
+def angle(s, f, e):
+    sf = math.hypot(f[0]-s[0], f[1]-s[1])
+    ef = math.hypot(f[0]-e[0], f[1]-e[1])
+    dot = (s[0]-f[0])*(e[0]-f[0]) + (s[1]-f[1])*(e[1]-f[1])
+    angle = math.degrees(math.acos(dot/(sf*ef)))
+    return angle
+
+
+def get_fingertips(palm_contour, im):
+    rect = cv2.minAreaRect(palm_contour)
+    palm_width = rect[1][0]
+    palm_height = rect[1][1]
+
+    hull = cv2.convexHull(palm_contour, returnPoints=False)
+    defects = cv2.convexityDefects(palm_contour, hull)
+
+    max_distance = 0.8 * palm_height
+    min_distance = 0.1 * palm_height
+    min_angle = 5
+    max_angle = 100
+
+    fingertips = []
+    min_x_fingertips = sys.maxint
+    last_fingertips = (0, 0)
+    for i in range(defects.shape[0]):
+        s, e, f, d = defects[i, 0]
+        start = tuple(palm_contour[s][0])
+        end = tuple(palm_contour[e][0])
+        far = tuple(palm_contour[f][0])
+
+        dist_sf = math.hypot(far[0] - start[0], far[1] - start[1])
+        good_sf = min_distance < dist_sf and dist_sf < max_distance
+        dist_ef = math.hypot(far[0] - end[0], far[1] - end[1])
+        good_ef = min_distance < dist_ef and dist_ef < max_distance
+        angle_esf = angle(start, far, end)
+        good_angle = min_angle < angle_esf and angle_esf < max_angle
+
+        good_finger_defects = good_sf and good_ef and good_angle
+        if good_finger_defects:
+            fingertips.append(start)
+            # cv2.line(im, start, far, (255, 0, 0), 3)
+            # cv2.line(im, far, end, (255, 0, 0), 3)
+            # cv2.circle(im, far, 5, (255, 255, 255), 5)
+
+            if start[0] < min_x_fingertips:
+                min_x_fingertips = start[0]
+                last_fingertips = end
+
+    fingertips.append(last_fingertips)
+    return fingertips
--- test/test_extract_image.py
+++ test/test_extract_image.py
@@ -1,26 +1,54 @@
 # coding=utf-8
+import math
 import numpy as np
 import cv2
-from extract_image import extract_image
+from extract_image import extract_image, angle
 
 im1 = cv2.imread("my_palm.jpg")
 im2 = cv2.imread("palm1.jpg")
 im3 = cv2.imread("palm2.jpg")
-palm_contour1 = extract_image(im1)
-palm_contour2 = extract_image(im2)
-palm_contour3 = extract_image(im3)
+palm_contour1, palm_center1, palm_size1, fingertips1 = extract_image(im1)
+palm_contour2, palm_center2, palm_size2, fingertips2 = extract_image(im2)
+palm_contour3, palm_center3, palm_size3, fingertips3 = extract_image(im3)
 
+# Draw contour
 cv2.drawContours(im1, [palm_contour1], -1, (0, 255, 0), 3)
 cv2.drawContours(im2, [palm_contour2], -1, (0, 255, 0), 3)
 cv2.drawContours(im3, [palm_contour3], -1, (0, 255, 0), 3)
 
-# Get convex hull
-hull1 = cv2.convexHull(palm_contour1)
-hull2 = cv2.convexHull(palm_contour2)
-hull3 = cv2.convexHull(palm_contour3)
-cv2.polylines(im1, np.int32([hull1]), 1, (0,0,255), 3)
-cv2.polylines(im2, np.int32([hull2]), 1, (0,0,255), 3)
-cv2.polylines(im3, np.int32([hull3]), 1, (0,0,255), 3)
+# Draw palm center
+cv2.circle(im1, palm_center1, 5, (0, 0, 255), 5)
+cv2.circle(im2, palm_center2, 5, (0, 0, 255), 5)
+cv2.circle(im3, palm_center3, 5, (0, 0, 255), 5)
+
+# Draw palm rectangle
+left_upper = (int(palm_center1[0] - palm_size1/2), int(palm_center1[1]-palm_size1/2))
+right_bottom = (int(palm_center1[0] + palm_size1/2), int(palm_center1[1]+palm_size1/2))
+cv2.rectangle(im1, left_upper, right_bottom, (0,255,0), 3)
+
+# Draw fingertips
+for fingertip in fingertips1:
+    cv2.circle(im1, fingertip, 5, (0, 0, 255), 5)
+for fingertip in fingertips2:
+    cv2.circle(im2, fingertip, 5, (0, 0, 255), 5)
+for fingertip in fingertips3:
+    cv2.circle(im3, fingertip, 5, (0, 0, 255), 5)
+
+# Draw convex hull
+# cv2.polylines(im1, np.int32([hull1]), 1, (0,0,255), 3)
+
+
+# # Contour centroid
+# M = cv2.moments(palm_contour1)
+# centroid_x = int(M['m10']/M['m00'])
+# centroid_y = int(M['m01']/M['m00'])
+# cv2.circle(im1,(centroid_x, centroid_y),5,[75,21,25],-1)
+
+#
+# (x,y),radius = cv2.minEnclosingCircle(palm_contour1[far_defects])
+# center = (int(x),int(y))
+# radius = int(radius)
+# cv2.circle(im1,center,radius,(0,255,0),2)
 
 cv2.imshow("images", np.hstack([im1, im2, im3]))
 # cv2.imshow("Output", im1)
